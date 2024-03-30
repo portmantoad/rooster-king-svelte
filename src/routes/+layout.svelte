821e85c73c8a267de58aadcc3e82f0e73656f1ef
portmantoad <portmantoad@users.noreply.github.com>
2024-03-30 13:13:27 -0700

    bug fixed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,9 @@
 	export let data;
 	import { isMuted } from '$lib/stores.js';
     import ScrollNag from '$lib/components/ScrollNag.svelte';
+    import { onMount } from 'svelte';
+
+
 
 	let pages = [
 		{route: '/lighthouse', name:'1. Lighthouse'},
@@ -17,19 +20,32 @@
 	];
 
 	let clickModal = (data.pathname == '/') ? false : true;
+	let clickModalDismissable = false;
+	let clickModalEscalations = 0;
 
-	let mutedStateActivelySet = false;
 	let canaryPaused = true;
 
 	$ : {
-		if (!mutedStateActivelySet) {
 			$isMuted = canaryPaused;
-		}
 	}
+
 	$isMuted = true;
 
 	let navVisible;
-	
+
+	const playVids = () => {
+		document.querySelectorAll(".autoplay").forEach((vid) => {
+	      vid.setAttribute("autoplay", "true");
+	      vid.play();
+	    });
+	}
+
+	const playVidsDelayed = (delay = 0) => setTimeout(() => {playVids()}, delay);
+
+	onMount(async () => {
+        setTimeout(() => {clickModalDismissable = true} , 900)
+    });
+
 </script>
 
 
@@ -150,6 +166,7 @@ nav a.active{ text-decoration:underline; }
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	z-index: 10000;
 }
 .clickModal__inner{
 	position: absolute;
@@ -162,8 +179,8 @@ nav a.active{ text-decoration:underline; }
 
 </style>
 
-<div class="navScrim" class:open={navVisible} on:click={()=>{navVisible = false;}} ></div>
-<nav class:open={navVisible} on:click={()=>{navVisible = !navVisible;}}><a href="/">ROOSTER KING</a>
+<div class="navScrim" class:open={navVisible} on:click={()=>{navVisible = false; playVidsDelayed(400);}} ></div>
+<nav class:open={navVisible} on:click={()=>{navVisible = !navVisible; playVidsDelayed(400);}}><a href="/">ROOSTER KING</a>
 	<ul>
 		{#each pages as page}
 	        <li><a class:active={data.pathname == page.route} href={page.route}>{page.name}</a></li>
@@ -187,68 +204,39 @@ nav a.active{ text-decoration:underline; }
 </div>
 
 {#key data.pathname}
-	<div in:fade={{ duration: 900, delay: 400 }} out:fade={{ duration: 200 }} style="overflow-x: clip;">
+	<div 
+		in:fade={{ duration: 0, delay: 400 }} 
+		out:fade={{ duration: 200 }} 
+		style="overflow-x: clip;" 
+		on:click={() => {playVidsDelayed(400);}}
+	>
 		<slot />
 	</div>
 	<ScrollNag></ScrollNag>
-	<video src="/img/lighthouse/title3.mp4" autoplay loop class="canary" bind:paused={canaryPaused} style="display: none;"></video>
+	<video src="/img/lighthouse/title3.mp4" class="canary autoplay" loop bind:paused={canaryPaused} style="display: none;"></video>
 {/key}
 
 
 {#if clickModal}
-<div class="clickModal" in:fade={{ duration: 900, delay: 400 }} out:fade={{ duration: 200 }} on:click={() => {
-	const vids = document.querySelectorAll(".autoplay");
-    vids.forEach((vid) => {
-      vid.setAttribute("autoplay", "true");
-      vid.play();
-    });
-    $isMuted = false;
-    clickModal = false;
+<div class="clickModal" out:fade={{ duration: 200 }} on:click={() => {
+
+	if (clickModalDismissable){
+	
+	    $isMuted = false;
+	    clickModal = false;
+	    playVids();
+	} else {
+		clickModalEscalations = clickModalEscalations + 1;
+	}
 }}>
 	<div class="clickModal__inner">
+		{#if clickModalEscalations === 0}
 		<p>Halt! By virtue of the divine edict set forth by the Apple Corporation, you must surrender one click.</p>
 		<button>Of course, my leige...</button> <button>Away, scoundrel!</button>
+		{:else}
+			<p>Another!</p>
+			<button>fiend, is {clickModalEscalations} not enough!?</button>
+		{/if}
 	</div>
 </div>
 {/if}
-
-		
-
-<!--   <style type="text/css">
-    .noizz{
-      --fps:12;
-      --width:64px;
-      --opacity: 1;
-      position:fixed; top:-10lvh; left:-10lvh; right:-10lvh; height: 120lvh; z-index: 1000;
-      background-image: url("/img/bluenoise.png");
-      pointer-events: none;
-      animation: calc(1s/var(--fps)*3) steps(3) infinite alternate noizz-x both,     
-      calc(1s/var(--fps)*2) steps(2) infinite alternate noizz-x both,
-      calc(1s/var(--fps)*5) steps(5) infinite alternate noizz-x both,
-      calc(1s/var(--fps)*7) steps(7) infinite alternate noizz-x both,
-      calc(1s/var(--fps)*3) steps(3) infinite alternate noizz-y both,     
-      calc(1s/var(--fps)*2) steps(2) infinite alternate noizz-y both,
-      calc(1s/var(--fps)*5) steps(5) infinite alternate noizz-y both,
-      calc(1s/var(--fps)*7) steps(7) infinite alternate noizz-y both
-      ;
-      animation-composition: add;
-      mix-blend-mode: color-dodge;
-      background-size: var(--width) auto;
-      opacity:calc(var(--opacity) / 3);
-}
-
-@keyframes noizz-x {
-  0% { transform: translateX(-2.5lvh); }
-  100%  { transform: translateX(2.5lvh); }
-}
-
-@keyframes noizz-y {
-  0% { transform: translateY(-2.5lvh); }
-  100%  { transform: translateY(2.5lvh); }
-}
-  </style>
-<div class="noizz" style="--fps:5; --width:64px; --opacity:.5;"></div>
-<div class="noizz" style="--fps:7; --width:89px; --opacity:.3;"></div>
-<div class="noizz" style="--fps:9; --width:121px; --opacity:.2;"></div>
-
- -->
